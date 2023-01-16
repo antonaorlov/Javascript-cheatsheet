7c7227e8953546e09ce54b8e06ec8490ee5eaca2
Antonorlov <66281524+antonaorlov@users.noreply.github.com>
2023-01-16 11:58:39 -0500

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -44,6 +44,67 @@ alert( NaN + 1 ); // NaN
 alert( 3 * NaN ); // NaN
 alert( "not a number" / 2 - 1 ); // NaN
 
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+
+//Typeof is an operator to see what type variable is 
+typeof undefined // "undefined"
+typeof 0 // "number"
+typeof 10n // "bigint"
+typeof true // "boolean"
+typeof "foo" // "string"
+typeof Symbol("id") // "symbol"
+typeof Math // "object"  (1) Built in object
+typeof null // "object"  (2)
+typeof alert // "function"  (3)
+
+Example
+let name = "Ilya";
+// the expression is a number 1
+alert( `hello ${1}` ); // hello 1
+// the expression is a string "name"
+alert( `hello ${"name"}` ); // hello name
+// the expression is a variable, embed it
+alert( `hello ${name}` ); // hello Ilya
+
+//Alert, Prompt, Confirm
+alert("Hello"); //shows message to user wait for them to close it
+
+result = prompt(title, [default]); //2 parameters, default is optional
+let age = prompt('How old are you?', 100);
+alert(`You are ${age} years old!`); // You are 100 years old!
+
+let isBoss = confirm("Are you the boss?"); //asks if ur boss to user if yes return true, if no then false  
+alert( isBoss ); // true if OK is pressed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
