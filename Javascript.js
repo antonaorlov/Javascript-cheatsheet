a9eb74189721ca6ae6364f4a71ace1d7da49c5ba
Antonorlov <66281524+antonaorlov@users.noreply.github.com>
2023-01-16 21:24:39 -0500

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -85,6 +85,145 @@ alert(`You are ${age} years old!`); // You are 100 years old!
 let isBoss = confirm("Are you the boss?"); //asks if ur boss to user if yes return true, if no then false  
 alert( isBoss ); // true if OK is pressed
 
+alert( 'Z' > 'A' ); // true
+alert( 'Glow' > 'Glee' ); // true
+alert( 'Bee' > 'Be' ); // true
+
+alert( '2' > 1 ); // true, string '2' becomes a number 2
+alert( '01' == 1 ); // true, string '01' becomes a number 1
+
+alert( null === undefined ); // false
+alert( null == undefined ); // true
+
+let i = 0;
+do {
+  alert( i );
+  i++;
+} while (i < 3);
+
+for (let i = 0; i < 3; i++) { // shows 0, then 1, then 2
+  alert(i);
+}
+
+let sum = 0;
+while (true) {
+  let value = +prompt("Enter a number", '');
+  if (!value) break; //break keywoard exits loop
+  sum += value;
+}
+alert( 'Sum: ' + sum );
+
+for (let i = 0; i < 10; i++) {
+  // if true, skip the remaining part of the body
+  if (i % 2 == 0) continue; //continues breaks current loop and starts new
+  alert(i); // 1, then 3, 5, 7, 9
+}
+
+let a = 2 + 2;
+switch (a) {
+  case 3:
+    alert( 'Too small' );
+    break;
+  case 4:
+    alert( 'Exactly!' );
+    break;
+  case 5:
+    alert( 'Too big' );
+    break;
+  default:
+    alert( "I don't know such values" );
+}
+
+//Local variables
+function showMessage() {
+  let message = "Hello, I'm JavaScript!"; // local variable
+  alert( message );
+}
+showMessage(); // Hello, I'm JavaScript!
+alert( message ); // <-- Error! The variable is local
+
+//Outer variables 
+let userName = 'John';
+function showMessage() {
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+showMessage(); // Hello, John
+
+//Function expressions is created when the execution reaches it and is usable only from that moment.
+let sayHi = function() {
+  alert( "Hello" );
+};
+
+function sayHi() {   // (1) create
+  alert( "Hello" );
+}
+let func = sayHi;    // (2) copy
+func(); // Hello     // (3) run the copy (it works)!
+sayHi(); // Hello    //     this still works too (why wouldn't it)
+
+//Callback functions
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); }, //if confirm yes 
+  function() { alert("You canceled the execution."); } //if no
+);
+
+//Arrow Functions
+let sum = (a, b) => a + b;
+/* This arrow function is a shorter form of:
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+alert( sum(1, 2) ); // 3
+
+let sayHi = () => alert("Hello!");
+sayHi();
+
+let sum = (a, b) => {  // the curly brace opens a multiline function
+  let result = a + b;
+  return result; // if we use curly braces, then we need an explicit "return"
+};
+alert( sum(1, 2) ); // 3
+
+// backtick quotes ` allow to split the string into multiple lines
+let str = `
+  ECMA International's TC39 is a group of JavaScript developers,
+  implementers, academics, and more, collaborating with the community
+  to maintain and evolve the definition of JavaScript.
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
